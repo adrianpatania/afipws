971e12e23c3f5084a585d5a0a0e50a7d5a63910f
adrian patania <adrianpatania@gmail.com>
2025-08-07 11:15:05 -0300

    Disable SSL VERIFY

--- lib/afipws/client.rb
+++ lib/afipws/client.rb
@@ -1,7 +1,11 @@
 module Afipws
   class Client
     def initialize savon_options
-      @savon = Savon.client savon_options.reverse_merge(soap_version: 2, ssl_version: :TLSv1_2)
+      @savon = Savon.client savon_options.reverse_merge(
+        soap_version: 2,
+        ssl_version: :TLSv1_2,
+        ssl_verify_mode: :none
+      )
     end
 
     def request action, body = nil
